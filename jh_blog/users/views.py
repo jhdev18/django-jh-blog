6c9efa5a3bd77336e35c13d32328e042381f272d
João Henrique <joaohca118@gmail.com>
2026-03-20 10:31:46 -0300

    adicionado registro de novos usuários

--- jh_blog/users/views.py
+++ jh_blog/users/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def registro(request):
-    return render(request, "users/registro_page.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:index")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/registro_page.html", {"form": form})
